7df5ad062d18f74dba263c530b5c9b17e6e556d6
Javin Leslie Lau <javinlau21@gmail.com>
2025-05-04 01:37:05 -0400

    enumeration with candidates

--- filters.rb
+++ filters.rb
@@ -3,14 +3,45 @@
 
 def find(id)
     # Your code Here
+    @candidates.find { |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
     # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
     # Your code Here
+    candidates.select do |candidate|
+      experienced?(candidate) &&
+      has_enough_github_points?(candidate) &&
+      knows_required_language?(candidate) &&
+      applied_recently?(candidate) &&
+      of_age?(candidate)
   end
+end
+
+def has_enough_github_points?(candidate)
+  candidate[:github_points] >= 100
+end
+
+def knows_required_language?(candidate)
+  candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python")
+end
+
+def applied_recently?(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+def of_age?(candidate)
+  candidate[:age] >= 18
+end
+
+def ordered_by_qualifications(candidates)
+  candidates.sort_by do |candidate|
+    [-candidate[:years_of_experience], -candidate[:github_points]]
+  end
+end
   
   # More methods will go below
